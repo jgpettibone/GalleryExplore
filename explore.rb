5313522f2b99f418db3fe24606051db012b226b4
Hack Reactor <hackreactor@Hacks-Mac-F2.local>
2014-02-10 11:35:34 -0800

    built the basic structure with angular and sinatra

---
+++ explore.rb
@@ -0,0 +1,144 @@
+require 'sinatra'
+require "sinatra/reloader" if development?
+require 'active_record'
+require 'digest/sha1'
+require 'pry'
+require 'uri'
+require 'open-uri'
+# require 'bcrypt'
+require 'tux'
+# # require 'nokogiri'
+
+###########################################################
+# Configuration
+###########################################################
+
+set :public_folder, File.dirname(__FILE__) + '/public'
+
+configure :development, :production do
+    ActiveRecord::Base.establish_connection(
+       :adapter => 'sqlite3',
+       :database =>  'db/dev.sqlite3.db'
+     )
+end
+
+# Handle potential connection pool timeout issues
+after do
+    ActiveRecord::Base.connection.close
+end
+
+# turn off root element rendering in JSON
+ActiveRecord::Base.include_root_in_json = false
+
+###########################################################
+# Models
+###########################################################
+# Models to Access the database through ActiveRecord.
+# Define associations here if need be
+# http://guides.rubyonrails.org/association_basics.html
+
+# class Link < ActiveRecord::Base
+#     has_many :clicks
+
+#     validates :url, presence: true
+
+#     before_save do |record|
+#         record.code = Digest::SHA1.hexdigest(url)[0,5]
+#     end
+# end
+
+# class Click < ActiveRecord::Base
+#     belongs_to :link, counter_cache: :visits
+# end
+
+# class User < ActiveRecord::Base
+#   has_secure_password
+
+#   def authenticate(password)
+#     self.password_digest == BCrypt::Engine.hash_secret(password, self.salt)
+#   end
+
+#   validates :username, presence: true, length: { maximum: 50 }, uniqueness: true
+#   validates :password, length: { minimum: 6 }
+#   before_create do |record|
+#     record.salt = BCrypt::Engine.generate_salt
+#     record.password_digest = BCrypt::Engine.hash_secret(record.password, record.salt)
+#     record.token = Digest::SHA1.hexdigest record.to_s
+#   end
+# end
+
+###########################################################
+# Routes
+###########################################################
+
+get '/' do
+    erb :index
+end
+
+# get '/links' do
+#     links = Link.order("created_at DESC")
+#     links.map { |link|
+#         link.as_json.merge(base_url: request.base_url)
+#     }.to_json
+# end
+
+# post '/links' do
+#     data = JSON.parse request.body.read
+#     uri = URI(data['url'])
+#     raise Sinatra::NotFound unless uri.absolute?
+#     link = Link.find_by_url(uri.to_s) ||
+#            Link.create( url: uri.to_s, title: get_url_title(uri) )
+#     link.as_json.merge(base_url: request.base_url).to_json
+# end
+
+# get '/:url' do
+#     link = Link.find_by_code params[:url]
+#     raise Sinatra::NotFound if link.nil?
+#     link.clicks.create!
+#     redirect link.url
+# end
+
+# post '/login' do
+#   data = JSON.parse request.body.read
+#   user = User.find_by_username data['username']
+#   if user.nil? or !user.authenticate data['password']
+#     {token: ''}.to_json
+#   else
+#     {token: user.token}.to_json
+#   end
+# end
+
+# post '/register' do
+#   data = JSON.parse request.body.read
+#   user = User.create( username: data['username'],
+#                       password: data['password'],
+#                       password_confirmation: data['passwordConfirmation'])
+#   if user.errors.any?
+#     {token: ''}.to_json
+#   else
+#     {token: user.token}.to_json
+#   end
+# end
+
+###########################################################
+# Utility
+###########################################################
+
+# def read_url_head url
+#     head = ""
+#     url.open do |u|
+#         begin
+#             line = u.gets
+#             next  if line.nil?
+#             head += line
+#             break if line =~ /<\/head>/
+#         end until u.eof?
+#     end
+#     head + "</html>"
+# end
+
+# def get_url_title url
+#     # Nokogiri::HTML.parse( read_url_head url ).title
+#     result = read_url_head(url).match(/<title>(.*)<\/title>/)
+#     result.nil? ? "" : result[1]
+# end
---
+++ public/client/app.js
@@ -0,0 +1,105 @@
+var galleryExploreApp = angular.module('galleryExploreApp', [
+  'ngRoute'
+]);
+
+
+
+// var shortlyApp = angular.module('shortlyApp', [
+//   'ngRoute', 'ngCookies'
+// ]);
+
+// shortlyApp.config(['$routeProvider',
+//   function($routeProvider, $cookieStore) {
+//   $routeProvider.
+//     when('/', function(){
+//       if($cookieStore.get('user')){
+//         return {
+//           templateUrl: 'client/partials/listUrls.html',
+//           controller: 'UrlListCtrl'
+//         };
+//       } else {
+//         return {
+//           templateUrl: 'client/partials/login.html',
+//           controller: 'LoginCtrl'
+//         };
+//       }
+//     }).
+//     when('/create', {
+//       templateUrl: 'client/partials/createUrl.html',
+//       controller: 'UrlCreateCtrl'
+//     }).
+//     when('/login', {
+//       templateUrl: 'client/partials/login.html',
+//       controller: 'LoginCtrl'
+//     }).
+//     when('/register', {
+//       templateUrl: 'client/partials/register.html',
+//       controller: 'RegisterCtrl'
+//     }).
+//     otherwise({
+//       redirectTo: '/'
+//     });
+// }]);
+
+// shortlyApp.controller('UrlListCtrl', function($scope, $http) {
+//   $http({
+//     method: 'GET',
+//     url: '/links'
+//   }).then(function(obj){
+//     $scope.links = obj.data;
+//   });
+//   $scope.setOrderBy = function(key, reverse) {
+//     $scope.orderBy = key;
+//     $scope.reverse = !reverse;
+//   };
+// });
+
+// shortlyApp.controller('UrlCreateCtrl', function($scope, $http) {
+//   $scope.shorten = function(url) {
+//     $http({
+//       method: 'POST',
+//       url: '/links',
+//       data: {url: url}
+//     }).then(function(data){
+//       console.log(data);
+//     });
+//   };
+// });
+
+// shortlyApp.controller('LoginCtrl', function($scope, $http, $cookieStore) {
+//   $scope.authenticate = function(username, password) {
+//     $http({
+//       method: 'POST',
+//       url: '/login',
+//       data: {username: username, password: password}
+//     }).success(function(data){
+//       $cookieStore.put('user', data.token);
+//       console.log($cookieStore.get('user'));
+//     });
+//   };
+// });
+
+// shortlyApp.controller('RegisterCtrl', function($scope, $http, $cookieStore) {
+//   $scope.register = function(username, password, passwordConfirmation) {
+//     $http({
+//       method: 'POST',
+//       url: '/register',
+//       data: {username: username, password: password, passwordConfirmation: passwordConfirmation}
+//     }).success(function(data){
+//       $cookieStore.put('user', data.token);
+//       console.log($cookieStore.get('user'));
+//     });
+//   };
+// });
+
+// // shortlyApp.service('UserService', function(){
+// //   var currentUser;
+// //   var self = this;
+// //   this.setCurrentUser = function(user) {
+// //     currentUser = user;
+// //   };
+// //   this.currentUser = function() {
+// //     return currentUser;
+// //   };
+// // });
+
