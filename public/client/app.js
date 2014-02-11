f58c5e1abaafbcfda0ba9cce0366efab3000abfc
Hack Reactor <jeanette.pettibone@gmail.com>
2014-02-11 14:21:03 -0800

    fixed the twitter bootstrap conflicts, started working on the tour creation

--- public/client/app.js
+++ public/client/app.js
@@ -31,12 +31,6 @@ galleryExploreApp.config(['$routeProvider',
 }]);
 
 
-// galleryExploreApp.service('ImageService', function() {
-//   return {
-//     saved: []
-//   }
-// });
-
 galleryExploreApp.controller('ImageDetailsController', function($scope, $http, $window, $location) {
 
   $scope.research = [
@@ -48,30 +42,32 @@ galleryExploreApp.controller('ImageDetailsController', function($scope, $http, $
   {id:86, src:"../images/l05-1-159.jpg", r:[{name:"Curator's Notes", path:"../research/test_paper.txt"},{name:"Research Paper", path:"../research/test_paper2.txt"}]}
   ];
 
+  // $scope.research = [
+  // {id:5, src:"../images/1980-73.jpg", r:[{name:"Curator's Notes", path:"../research/1980.73.pdf"},{name:"Research Paper", path:"../research/master-drum.pdf"}]},  
+  // {id:35, src:"../images/2009-8.jpg", r:[{name:"Curator's Notes", path:"../research/2009.8.pdf"}, {name:"Curator's Notes 2", path:"../research/Art_and_Oracle.pdf"}, {name:"Study Group Notes", path:"../research/AOA_Study_GALLERY_WALKTHROUGH.doc"},{name:"Research Paper", path:"../research/2009.8_Harp_Pettibone.doc"}]},
+  // {id:58, src:"../images/2001-62-9.jpg", r:[{name:"Curator's Notes", path:"../research/2001.62.6.pdf"}, {name:"Curator's Notes 2", path:"../research/2001.62.6SlitDrum.pdf"},{name:"Research Paper", path:"../research/slit-drum.pdf"}]},
+  // {id:64, src:"../images/2007-44-13.jpg", r:[{name:"Curator's Notes", path:"../research/2007.44.13.pdf"},{name:"Research Paper", path:"../research/hand-drums.pdf"}]},
+  // {id:65, src:"../images/2007-44-14.jpg", r:[{name:"Curator's Notes", path:"../research/2007.44.14.pdf"},{name:"Research Paper", path:"../research/hand-drums.pdf"}]},
+  // {id:86, src:"../images/l05-1-159.jpg", r:[{name:"Curator's Notes", path:"../research/test_paper.txt"},{name:"Research Paper", path:"../research/slit-drum.pdf"}]}
+  // ];
+
+
   $http({
     method: 'GET',
     url: '/details'
   })
   .then(function(obj){
     $scope.image = obj.data;
-    // console.log('image = ', $scope.image.src);
     for (var i = 0; i < $scope.research.length; i++) {
-      // console.log('other src = ', $scope.research[i].src);
       if ($scope.research[i].src === $scope.image.src) {
         $scope.image.research = $scope.research[i].r;
         break;
       }
-      // else {
-      //   $scope.image.research =[{name:"Research not yet posted", path:nil}];
-      // } 
     }
-    // console.log('research = ', $scope.image.research);
   });
 
   $scope.showDoc = function(path){
     $window.open(path);
-    // $location.path("http://www.google.com");
-    // $scope.save();
   };
 
   $scope.addTag = function(tag, src) {
@@ -79,16 +75,13 @@ galleryExploreApp.controller('ImageDetailsController', function($scope, $http, $
       method: 'POST',
       url: '/tags',
       data: {src: src, tags: tag}
-    }).then(function(){
-      // $scope.save();
-      $scope.tag = "";
-    // $scope.image.tags;
+    }).then(function(data){
+      console.log(data, data.data['tags']);
+      $scope.image.tags = data.data['tags'];
     });
   };
 
   $scope.addImage = function(img) {
-    // console.log('saved ', img);
-    // ImageService.saved.push(img);
 
     $http({
       method: 'POST',
@@ -99,7 +92,6 @@ galleryExploreApp.controller('ImageDetailsController', function($scope, $http, $
   };
 
   $scope.save = function() {
-    // $location.path('/details');
     $location.path("http://www.google.com");
   }
 
@@ -112,10 +104,6 @@ galleryExploreApp.controller('TourViewController', function($scope, $window, $lo
     {tourid: 2, tourname: 'Oceania - Three In 30', museum:'deyoung'},
     {tourid: 3, tourname: 'Female Figures, Female Rituals, Female Artists', museum:'deyoung'}
   ];
-
-//1 - dogon, yipwon, slit drum, stela
-//2 - female cult hook, string bag, yipwon, slit drum, female house post 
-//3 - female cult hook, female giving birth, mourning costume, bilum, female house post, sowei mask, odundo pot, harp, bowstand
 
   $scope.tourimage = [
   {tourid: 2, order:1, src:"../images/l05-1-67.jpg"},
@@ -131,13 +119,7 @@ galleryExploreApp.controller('TourViewController', function($scope, $window, $lo
 
   $scope.selectTour = function(tour){
     $window.open('/#/onetour');
-    // console.log('Tour! ', tour);
-    // $scope.save();
   };
-
-  // $scope.save = function() {
-  //   $location.path('/tourslist');
-  // };
 
 });
 
@@ -150,9 +132,7 @@ galleryExploreApp.controller('OneTourViewController', function($scope, $location
     {tourid: 3, tourname: 'Female Figures, Female Rituals, Female Artists', museum:'deyoung'}
   ];
 
-//1 - dogon, yipwon, slit drum, stela
 //2 - female cult hook, string bag, yipwon, slit drum, female house post 
-//3 - female cult hook, female giving birth, mourning costume, bilum, female house post, sowei mask, odundo pot, harp, bowstand
 
   $scope.tourimage = [
   {tourid: 2, order:1, src:"../images/l05-1-67.jpg"},
@@ -167,20 +147,13 @@ galleryExploreApp.controller('OneTourViewController', function($scope, $location
   ];
 
   $scope.selectTour = function(tour){
-    // console.log('Tour! ', tour);
-    // $scope.save();
   };
-
-  // $scope.save = function() {
-  //   $location.path('/tourslist');
-  // };
 
 });
 
 galleryExploreApp.controller('TourCreateController', function($scope, $http) {
 
-  // $scope.tourimages = ImageService.saved;
-  // console.log($scope.tourimages);
+  $scope.tour = {};
 
   $http({
     method: 'GET',
@@ -190,6 +163,10 @@ galleryExploreApp.controller('TourCreateController', function($scope, $http) {
     console.log($scope.tourimages);
   });
 
+  $scope.submitName = function(tourname){
+    $scope.tour.tourname = tourname;
+    $scope.tourname = '';
+  }
 
 });
 
@@ -213,7 +190,6 @@ galleryExploreApp.controller('ImageGalleryController', function($scope, $http, $
 
   $scope.save = function() {
     $window.open('/#/details');
-    // $location.path('/details');
   }
 
 });
