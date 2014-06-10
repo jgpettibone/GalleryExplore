a93b1620d04dda9e81d46995f08ad5dd862a2c35
jgpettibone <jeanette.pettibone@gmail.com>
2014-06-10 14:47:40 -0700

    cleaned up save function

--- public/client/imagegallery.js
+++ public/client/imagegallery.js
@@ -1,11 +1,12 @@
 galleryExploreApp.controller('ImageGalleryController', function($scope, $http, $window, $location) {
+
   $http({
     method: 'GET',
     url: '/images'
   }).then(function(obj){
     $scope.images = obj.data;
   });
-  
+
   $scope.showDetails = function(src) {
     $http({
       method: 'POST',
