60ab4acaee8b6240b1c10c104f66a9d8a705ef8a
jgpettibone <jeanette.pettibone@gmail.com>
2014-06-16 23:12:15 -0700

    updated tag and talkingpts methods to deal with the interaction needed for muliples

--- public/client/imagedetails.js
+++ public/client/imagedetails.js
@@ -1,31 +1,39 @@
 galleryExploreApp.controller('ImageDetailsController', function($scope, $http, $window, $location, ImageHTTPService) {
 
+  $scope.notes = {};
+  $scope.tags = {};
+
   ImageHTTPService.getDetails().then(function(data){
     $scope.image = data;
-    $scope.notes = $scope.image.talkingpts;
+    $scope.notes[$scope.image.id] = $scope.image.talkingpts;
   });
 
-  $scope.addTag = function(tag, src) {
-    ImageHTTPService.addTag(tag, src).then(function(data){
+  $scope.addTag = function(tag, id) {
+    ImageHTTPService.addTag(tag, id).then(function(data){
       $scope.image.tags = data['tags'];
     });
     $scope.tag = null;
   };
 
-  $scope.addTalkingPts = function(notes, src) {
-    ImageHTTPService.addTalkingPts(notes, src).then(function(data){
-      $scope.image.talkingpts = data['talkingpts'];
-      $scope.notes = data['talkingpts'];
+  $scope.addTalkingPts = function(notes, id) {
+    ImageHTTPService.addTalkingPts(notes, id).then(function(data){
+      $scope.image = data;
+      $scope.notes[$scope.image.id] = $scope.image.talkingpts;
     });
   };
 
   $scope.addImage = function(img) {
     ImageHTTPService.addImage(img);
+    $location.path('/create').replace;
   };
 
-  $scope.expand = function(img) {
-    console.log('hello');
-    // $window.open(img);
+  // $scope.expand = function(img) {
+  //   console.log('hello');
+  //   // $window.open(img);
+  // };
+
+  $scope.close = function() {
+    $location.path('/').replace;
   };
 
 });
