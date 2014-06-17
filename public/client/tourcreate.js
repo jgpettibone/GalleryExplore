459088c11892d1abb4d2d8852f21de1b4e26ecaf
jgpettibone <jeanette.pettibone@gmail.com>
2014-06-16 23:15:54 -0700

    updated tag and talkingpts to deal with multiple images

--- public/client/tourcreate.js
+++ public/client/tourcreate.js
@@ -1,18 +1,44 @@
 galleryExploreApp.controller('TourCreateController', function($scope, $location, $http, ImageHTTPService) {
 
   $scope.tour = {};
-  $scope.message = "";
+  $scope.message = '';
+  $scope.notes = {};
+  $scope.tourimages;
 
   ImageHTTPService.getSaveds().then(function(data){
     $scope.tourimages = data;
-    $scope.notes = $scope.tourimages.talkingpts;
+    populateTalkingPts($scope.tourimages);
     checkTourLength($scope.tourimages);
   });
 
-  $scope.submitName = function(tourname){
-    $scope.tour.tourname = tourname;
-    $scope.tourname = '';
-  }
+  $scope.saveTour = function(tourname){
+    console.log('tourname = ', $scope.tourname);
+    if (!$scope.tourname) { 
+      $scope.message = 'This tour needs a name before it can be saved';
+    }
+    else {
+      $scope.message = '';
+    }
+  };
+
+  $scope.addTag = function(tag, id) {
+    ImageHTTPService.addTag(tag, id).then(function(data){
+      var tours = $scope.tourimages;
+      for (var i = 0; i < tours.length; i++) {
+        if (tours[i].id === id) tours[i].tags = data['tags']
+      }      
+    });
+    $scope.tag = null;
+  };
+
+
+  $scope.addTalkingPts = function(notes, id) {
+    console.log('notes = ', notes);
+    ImageHTTPService.addTalkingPts(notes, id).then(function(data){
+      $scope.notes[$scope.image.id] = data['talkingpts'];
+    });
+  };
+
 
   $scope.removeImage = function(image) {
     ImageHTTPService.removeImage(image).then(function(data){
@@ -31,9 +57,19 @@ galleryExploreApp.controller('TourCreateController', function($scope, $location,
     return image.order;
   };
 
-  var checkTourLength = function(tour) {
-    if (tour.length === 0)
-      $scope.message = 'Use Explore Images to add art to the tour.';
+  $scope.findArt = function() {
+    $location.path('/').replace;
+  };
+
+  var checkTourLength = function(tours) {
+    if (tours.length === 0)
+      $scope.message = 'Click on Find More Artwork to find art to add to the tour.';
+  };
+
+  var populateTalkingPts = function(tours) {
+    for (var i = 0; i < tours.length; i++) {
+      $scope.notes[tours[i].id] = tours[i].talkingpts;
+    }
   };
 
 });
