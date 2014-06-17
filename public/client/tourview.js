49fbb8be3606d8dad28ad39355aaf70093f842f8
jgpettibone <jeanette.pettibone@gmail.com>
2014-06-17 08:17:34 -0700

    implemented functions to view the tour that was clicked on

--- public/client/tourview.js
+++ public/client/tourview.js
@@ -1,25 +1,12 @@
-galleryExploreApp.controller('TourViewController', function($scope, $window, $location) {
+galleryExploreApp.controller('TourViewController', function($scope, $window, $location, ImageHTTPService, TourService) {
 
-  $scope.tours = [
-    {tourid: 1, tourname: 'AOA Masterpieces - Three Pieces In 30 Minutes', museum:'deyoung'},
-    {tourid: 2, tourname: 'Oceania - Three Pieces In 30 Minutes', museum:'deyoung'},
-    {tourid: 3, tourname: 'Female Figures, Female Rituals, Female Artists', museum:'deyoung'}
-  ];
-
-  $scope.tourimage = [
-  {tourid: 2, order:1, src:"../images/l05-1-67.jpg"},
-  {tourid: 2, order:2, src:"../images/2007-44-79.jpg"},
-  {tourid: 2, order:3, src:"../images/2000-172-1.jpg"},
-  {tourid: 2, order:4, src:"../images/2001-62-9.jpg"},
-  {tourid: 2, order:5, src:"../images/l05-1-19.jpg"}
-  ];
-
-  $scope.talkingpts = [
-
-  ];
+  ImageHTTPService.getTours().then(function(data){
+    $scope.tours = data;
+  });
 
   $scope.selectTour = function(tour){
-    $window.open('/#/onetour');
+    TourService.setTour(tour);
+    $location.path('/onetour').replace;
   };
 
 });
